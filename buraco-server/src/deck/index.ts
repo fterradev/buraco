9b831f5dc078f21cdd73e8d838a139cd1f556b18
Fernando Terra <fterra@daitan.com>
2020-11-01 23:53:26 -0300

    feat: convert function game into class Game

--- buraco-server/src/deck/index.ts
+++ buraco-server/src/deck/index.ts
@@ -13,7 +13,7 @@ let cardId = 0;
 for (let back = 0; back < 2; back++) {
   for (let suit = 0; suit < 4; suit++) {
     for (let rank = 0; rank < 13; rank++) {
-      const card: Card = {id: cardId, back, suit, rank};
+      const card: Card = { id: cardId, back, suit, rank };
       deck.push(card);
       cardId++;
     }
@@ -21,3 +21,18 @@ for (let back = 0; back < 2; back++) {
 }
 
 export default deck;
+
+export const getNewDeck = () => {
+  const deck: CardSet = [];
+  let cardId = 0;
+  for (let back = 0; back < 2; back++) {
+    for (let suit = 0; suit < 4; suit++) {
+      for (let rank = 0; rank < 13; rank++) {
+        const card: Card = { id: cardId, back, suit, rank };
+        deck.push(card);
+        cardId++;
+      }
+    }
+  }
+  return deck;
+}
